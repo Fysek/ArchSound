81c21893826894f828c0a6a9b2c73726d3025cc9
Mateusz Dyrdol <mateuszdyrdol@gmail.com>
2021-06-25 12:45:00 +0200

    Added samples config + cleaned samples in skatepark zone

--- sliderScanner.py
+++ sliderScanner.py
@@ -6,8 +6,12 @@ import time
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 #Only SIX ports are used on each MCP3008
+#Read values 
+#
+
 
 DEBUG = 1
+LOW_VALUE_BOARDER = 64
 
 class SliderScanner():
     def __init__(self, readInterval=1.0):
@@ -19,25 +23,26 @@ class SliderScanner():
         self.__mcp1 = MCP.MCP3008(self.__spi1, self.__cs0_1)
         self.__timeInterval = readInterval
         self.__timeInterval = 0.2
-        self.__channels = [0]*16
-        self.__values = [0]*16
-        self.__currentActiveChannel = None
-        self.__channels[0] = AnalogIn(self.__mcp0, MCP.P0)
-        self.__channels[1] = AnalogIn(self.__mcp0, MCP.P1)
-        self.__channels[2] = AnalogIn(self.__mcp0, MCP.P2)
-        self.__channels[3] = AnalogIn(self.__mcp0, MCP.P3)
-        self.__channels[4] = AnalogIn(self.__mcp0, MCP.P4)
-        self.__channels[5] = AnalogIn(self.__mcp0, MCP.P5)
-        self.__channels[6] = AnalogIn(self.__mcp0, MCP.P6)
-        self.__channels[7] = AnalogIn(self.__mcp0, MCP.P7)
-        self.__channels[8] = AnalogIn(self.__mcp1, MCP.P0)
-        self.__channels[9] = AnalogIn(self.__mcp1, MCP.P1)
-        self.__channels[10] = AnalogIn(self.__mcp1, MCP.P2)
-        self.__channels[11] = AnalogIn(self.__mcp1, MCP.P3)
-        self.__channels[12] = AnalogIn(self.__mcp1, MCP.P4)
-        self.__channels[13] = AnalogIn(self.__mcp1, MCP.P5)
-        self.__channels[14] = AnalogIn(self.__mcp1, MCP.P6)
-        self.__channels[15] = AnalogIn(self.__mcp1, MCP.P7)
+        self.__channels0 = [0]*6
+        self.__channels1 = [0]*5
+        self.__values = [0]*11
+        self.__activeTable = [0]*11
+        self.__moveTable = [0]*11
+        self.__currentActiveChannel = None #Number from 1-11
+        #MCP3008 #1
+        self.__channels0[0] = AnalogIn(self.__mcp0, MCP.P0)
+        self.__channels0[1] = AnalogIn(self.__mcp0, MCP.P1)
+        self.__channels0[2] = AnalogIn(self.__mcp0, MCP.P2)
+        self.__channels0[3] = AnalogIn(self.__mcp0, MCP.P3)
+        self.__channels0[4] = AnalogIn(self.__mcp0, MCP.P4)
+        self.__channels0[5] = AnalogIn(self.__mcp0, MCP.P5)
+        #MCP3008 #2
+        self.__channels1[0] = AnalogIn(self.__mcp1, MCP.P0)
+        self.__channels1[1] = AnalogIn(self.__mcp1, MCP.P1)
+        self.__channels1[2] = AnalogIn(self.__mcp1, MCP.P2)
+        self.__channels1[3] = AnalogIn(self.__mcp1, MCP.P3)
+        self.__channels1[4] = AnalogIn(self.__mcp1, MCP.P4)
+
     
     def __debug_message(self, message):
         if DEBUG:
@@ -45,19 +50,23 @@ class SliderScanner():
     
     def readValues(self):
         self.__debug_message("-----------------")
+        #Scanning first MCP3008
         for i in range(6):
-            self.__values[i] = self.__channels[i].value
+            self.__values[i] = self.__channels0[i].value
    
         self.__debug_message(self.__values[0:6]) 
         time.sleep(self.__timeInterval) 
-    
-        for i in range(6):
-            self.__values[i+8] = self.__channels[i+8].value
+        #Scanning second MCP3008
+        for i in range(5):
+            self.__values[i+6] = self.__channels1[i].value
  
-        self.__debug_message(self.__values[8:14])
+        self.__debug_message(self.__values[6:11])
         time.sleep(self.__timeInterval)  
-  
-  
-  
-  
-  
+
+    def evaluateValues(self):
+        for i in range(11):
+            if self.__values[i] > LOW_VALUE_BOARDER:
+                self.__activeTable[i] = True 
+            
+         
+            
