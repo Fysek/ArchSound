5f7980897a4996377e8a4fce47ff49adca1ee1ca
Mateusz Dyrdol <mateuszdyrdol@gmail.com>
2021-06-25 12:12:40 +0200

    added samples + working on slider

--- sliderScanner.py
+++ sliderScanner.py
@@ -5,18 +5,23 @@ import board
 import time
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+#Only SIX ports are used on each MCP3008
+
+DEBUG = 1
 
 class SliderScanner():
-    def __init__(self):
+    def __init__(self, readInterval=1.0):
         self.__spi0 = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
         self.__spi1 = busio.SPI(clock=board.SCK_1, MISO=board.MISO_1, MOSI=board.MOSI_1)
         self.__cs0_0 = digitalio.DigitalInOut(board.D8)
         self.__cs0_1 = digitalio.DigitalInOut(board.D18)
         self.__mcp0 = MCP.MCP3008(self.__spi0, self.__cs0_0)
         self.__mcp1 = MCP.MCP3008(self.__spi1, self.__cs0_1)
-        self.__timeInterval = 0.5
+        self.__timeInterval = readInterval
+        self.__timeInterval = 0.2
         self.__channels = [0]*16
         self.__values = [0]*16
+        self.__currentActiveChannel = None
         self.__channels[0] = AnalogIn(self.__mcp0, MCP.P0)
         self.__channels[1] = AnalogIn(self.__mcp0, MCP.P1)
         self.__channels[2] = AnalogIn(self.__mcp0, MCP.P2)
@@ -34,18 +39,25 @@ class SliderScanner():
         self.__channels[14] = AnalogIn(self.__mcp1, MCP.P6)
         self.__channels[15] = AnalogIn(self.__mcp1, MCP.P7)
     
+    def __debug_message(self, message):
+        if DEBUG:
+            print(message)
     
     def readValues(self):
-        print("-----------------")
-        for i in range(8):
+        self.__debug_message("-----------------")
+        for i in range(6):
             self.__values[i] = self.__channels[i].value
    
-        print(self.__values[0:2]) 
+        self.__debug_message(self.__values[0:6]) 
         time.sleep(self.__timeInterval) 
     
-        for i in range(8):
+        for i in range(6):
             self.__values[i+8] = self.__channels[i+8].value
  
-        print(self.__values[8:10])
+        self.__debug_message(self.__values[8:14])
         time.sleep(self.__timeInterval)  
+  
+  
+  
+  
   
