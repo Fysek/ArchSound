f9c26541b42909fdc633b9d3d42d1fa036acadb1
Mateusz Dyrdol <mateuszdyrdol@gmail.com>
2021-06-26 20:00:22 +0200

    Fixed moves forward and backwards

--- sliderScanner.py
+++ sliderScanner.py
@@ -8,11 +8,15 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 #Only SIX ports are used on each MCP3008
 #Read values 
-#
+# TO DO:
+# - block channel after gooing backwards until it gos back to 0 
 
 
 DEBUG = 1
 LOW_VALUE_BORDER = 64
+UNACTIVE_CHANNEL = 0
+MOVING_DIFF = 200
+MOVING_BACK_DIFF = 400
 
 class SliderScanner():
     def __init__(self, readInterval=1.0):
@@ -28,10 +32,8 @@ class SliderScanner():
         self.__channels1 = [0]*5
         self.__values = [0]*11
         self.__move = 0
-        self.__activeTable = [False]*11
         self.__currentActiveChannel = 0 #Number from 1-11
-        self.__lastReadingsActiveChannel = collections.deque(maxlen=10)
-        self.__lastReadingsActiveChannel = [0]*10
+        self.__lastReadingsActiveChannel = collections.deque([0,0,0,0,0,0,0,0,0,0], maxlen=10)
         #MCP3008 #1
         self.__channels0[0] = AnalogIn(self.__mcp0, MCP.P0)
         self.__channels0[1] = AnalogIn(self.__mcp0, MCP.P1)
@@ -56,11 +58,20 @@ class SliderScanner():
         Deque stores new values at the end
         Comparing last and penultimate reading
         """
-        if(self.__lastReadingsActiveChannel[-1] > self.__lastReadingsActiveChannel[-2]):
+        if(self.__lastReadingsActiveChannel[-1] - self.__lastReadingsActiveChannel[-2] > MOVING_DIFF):
             return True
         else:
             return False
-            
+    
+    def __isMovingBackwards(self):
+        """
+        If different between two last readings is bigger than MOVING_BACK_DIFF then user is going backwards
+        """
+        if(self.__lastReadingsActiveChannel[-2] - self.__lastReadingsActiveChannel[-1] > MOVING_BACK_DIFF):
+            return True
+        else:
+            return False
+      
     def __isActiveTenTimes(self):
         """
         Checks if the current channel is active for last ten measurements
@@ -81,7 +92,7 @@ class SliderScanner():
         time.sleep(self.__timeInterval)
         
         ### DEBUG  
-        self.__debug_message(str(self.__values[0:6]) + ' | ' + str(self.__values[6:11]))
+        self.__debug_message('All channels: ' + str(self.__values[0:6]) + ' | ' + str(self.__values[6:11]))
         ###
        
     def evaluateValues(self):
@@ -90,28 +101,26 @@ class SliderScanner():
         Return move flag
         """
         # No channel assigned
-        if self.__currentActiveChannel == 0:
+        if self.__currentActiveChannel == UNACTIVE_CHANNEL:
             for i in range(11):
                 if self.__values[i] > LOW_VALUE_BORDER:
-                    #self.__activeTable[i] = True # Do we need it?
-                    self.__currentActiveChannel = i+1                   
-                #else:
-                    #self.__activeTable[i] = False     
+                    self.__currentActiveChannel = i+1 
+                    self.__lastReadingsActiveChannel.append(self.__values[i])                  
+    
         else: ## Channel already assigned
-        
             currentReading = self.__values[self.__currentActiveChannel-1]
             self.__lastReadingsActiveChannel.append(currentReading)
             
-            if (currentReading > LOW_VALUE_BORDER): #active
+            if (currentReading > LOW_VALUE_BORDER): #active, channel stays, move depends, moving backwards cleans the
                 self.__move = self.__isMoving()
+                if self.__isMovingBackwards(): 
+                    self.__currentActiveChannel = UNACTIVE_CHANNEL
             else: # not active
-                self.__move = 0
+                self.__move = False
                 if not self.__isActiveTenTimes():
-                    self.__currentActiveChannel = 0
-                    self.__lastReadingsActiveChannel = [0]*10
+                    self.__currentActiveChannel = UNACTIVE_CHANNEL
                  
         ### DEBUG   
-        self.__debug_message("Active table: " + str(self.__activeTable))
         self.__debug_message("Current active channel: " + str(self.__currentActiveChannel))
         self.__debug_message("Last readings: " + str(self.__lastReadingsActiveChannel))      
         self.__debug_message("Move: " + str(self.__move))
